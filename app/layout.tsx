12a41f383d1b42b58ecec9f02dc09614872cebc5
David Martinez Rodríguez <321davidmr123@gmail.com>
2025-06-13 02:38:13 -0500

    metadata improved

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,15 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "Spotify Now Playing", 
-  description: "See what's currently playing on Spotify.",
+  title: "Spotify Feature", 
+  description: "Currently and recently played on Spotify.",
   // You can also add an 'icons' property for favicons:
   // icons: {
   //   icon: '/favicon.ico', // Default icon
   //   apple: '/apple-touch-icon.png', // For Apple devices
   //   shortcut: '/favicon-16x16.png', // Or other sizes
   // },
-    generator: 'v0.dev'
+    generator: 'davidmr'
 }
 
 export default function RootLayout({
